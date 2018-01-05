7bdf39b468877163bc2d6350991edf7d205e5ad5
shravannayak <shravan.nayak@prueba.io>
2018-01-05 17:31:20 +0530

    SQLite
    adding SQLite database

---
+++ HelloWorld/HelloWorld/HelloWorld.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using HelloWorld.Droid;
+using System.Runtime.CompilerServices;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace HelloWorld.Droid
+{
+	public class SQLiteDb : ISQLiteDb
+	{
+		public SQLiteAsyncConnection GetConnection()
+		{
+			var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+			return new SQLiteAsyncConnection(path);
+		}
+	}
+}
+
---
+++ HelloWorld/HelloWorld/HelloWorld/NewPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    public class Recipe
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Name { get; set; }
+    }
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewPage : ContentPage
+	{
+        private SQLiteAsyncConnection _connection;
+        private ObservableCollection<Recipe> _recipes;
+		public NewPage ()
+		{
+			InitializeComponent ();
+            _connection = DependencyService.Get<ISQLiteDb>().GetConnection();
+        }
+
+        async protected override void OnAppearing()
+        {
+            await _connection.CreateTableAsync<Recipe>();
+            var recipes=await _connection.Table<Recipe>().ToListAsync();
+            _recipes=new ObservableCollection<Recipe>(recipes);
+            recipesListView.ItemsSource = _recipes;
+            base.OnAppearing();
+        }
+
+        async void OnAdd(object sender, System.EventArgs e)
+        {
+            var recipe = new Recipe { Name = "Recipe " + DateTime.Now.Ticks };
+            await _connection.InsertAsync(recipe);
+            _recipes.Add(recipe);
+        }
+
+        void OnUpdate(object sender, System.EventArgs e)
+        {
+        }
+
+        void OnDelete(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
