21e45be2e170b7e87d09b6a98fcce8a450aa13cc
shravannayak <shravan.nayak@prueba.io>
2018-01-05 15:31:28 +0530

    ApplicationProperties
    Used data storage within the application
    using Application.Properties

--- HelloWorld/HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/HelloWorld/App.xaml.cs
@@ -9,11 +9,13 @@ namespace HelloWorld
 {
 	public partial class App : Application
 	{
-		public App ()
+        private const string TitleKey = "Name";
+        private const string NotificationsEnabledKey = "NotificationsEnabled";
+        public App ()
 		{
 			InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NewPracticePage();
             /*{
                 BarBackgroundColor = Color.Gray,
                 BarTextColor=Color.White
@@ -35,5 +37,33 @@ namespace HelloWorld
 		{
 			// Handle when your app resumes
 		}
+
+        public string Title
+        {
+            get
+            {
+                if (Properties.ContainsKey(TitleKey))
+                    return Properties[TitleKey].ToString();
+                return "";
+            }
+            set
+            {
+                Properties[TitleKey] = value;
+            }
+        }
+
+        public bool NotificationsEnabled
+        {
+            get
+            {
+                if (Properties.ContainsKey(NotificationsEnabledKey))
+                    return (bool)Properties[NotificationsEnabledKey];
+                return false;
+            }
+            set
+            {
+                Properties[NotificationsEnabledKey] = value;
+            }
+        }
 	}
 }
