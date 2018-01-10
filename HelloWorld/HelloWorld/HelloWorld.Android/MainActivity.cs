9ec0d796384409ae8870cad460d30ab34859b955
shravannayak <shravan.nayak@prueba.io>
2018-01-10 14:51:23 +0530

    Application Name changed
    App Name Renamed to "AppName"

--- HelloWorld/HelloWorld/HelloWorld.Android/MainActivity.cs
+++ HelloWorld/HelloWorld/HelloWorld.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace HelloWorld.Droid
 {
-    [Activity(Label = "HelloWorld", Icon = "@drawable/slack", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "NewApp", Icon = "@drawable/slack", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
--- HelloWorld/HelloWorld/HelloWorld.Android/SplashActivity.cs
+++ HelloWorld/HelloWorld/HelloWorld.Android/SplashActivity.cs
@@ -13,7 +13,7 @@ using Android.Widget;
 namespace HelloWorld.Droid
 {
     [Activity(Theme ="@style/Theme.Splash",
-        Label = "SplashActivity", MainLauncher = true,NoHistory =true)]
+        MainLauncher = true,NoHistory =true)]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
